37b2f2d843296456eeaef1cc1a9be32bf9c11ec4
Malick Aralan Home <malickarsalan@hotmail.com>
2021-07-10 16:15:01 +0500

    At last Lesson 2 Completed

--- 2. Architecture Consideration for Cloud Native Applications/11. Endpoints for Application Status and Logging/exercises/python-helloworld/app.py
+++ 2. Architecture Consideration for Cloud Native Applications/11. Endpoints for Application Status and Logging/exercises/python-helloworld/app.py
@@ -1,5 +1,7 @@
+import logging
 from flask import Flask
 from flask import json
+import logging
 
 app = Flask(__name__)
 
@@ -11,6 +13,7 @@ def status():
             mimetype='application/json'
     )
 
+    app.logger.info('Status request successful')
     return response
 
 @app.route('/metrics')
@@ -20,12 +23,16 @@ def metrics():
             status=200,
             mimetype='application/json'
     )
-
+    app.logger.info('Metrics request successful')
     return response
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request successfull')
     return "Hello World!"
 
+
+
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
